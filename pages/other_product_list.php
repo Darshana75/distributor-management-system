f8a41f91813d2c958df1a87a837d7423e6bacde7
Darshana75 <84192420+Darshana75@users.noreply.github.com>
2025-04-10 09:30:46 +0530

    Some changes

--- pages/other_product_list.php
+++ pages/other_product_list.php
@@ -10,7 +10,7 @@
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">factory product list</li>
+              <li class="breadcrumb-item active">factory Product list</li>
             </ol>
             </div><!-- /.col -->
             </div><!-- /.row -->
