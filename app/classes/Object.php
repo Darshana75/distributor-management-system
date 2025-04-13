84032947ba591a2c63847f49656a279efedb71f7
Isuru-Pavithra-Hapuarachchi <iphapuarachchi@gmail.com>
2025-04-13 08:39:08 +0530

    sell functionallity done

--- app/classes/Object.php
+++ app/classes/Object.php
@@ -170,78 +170,72 @@ function convertDate($value=''){
 	return $newDate;
 }
 
-// sell invoice create fucntion
-public function storeCustomerOrderInvoice($invoice_number,$customer_name ,$orderdate,$find_customer_name,$total_quantity,$orderQuantity,$price,$totalPrice,$pro_name,$pid,$subtotal,$discount,$prev_due,$netTotal,$paidBill,$dueBill,$payMethode ){
-		
-		$pre_stmt = $this->pdo->prepare("INSERT INTO `invoice`(`invoice_number`,`customer_id`,`customer_name`, `order_date`, `sub_total`, `discount`,`pre_cus_due`, `net_total`, `paid_amount`, `due_amount`, `payment_type`) VALUES (?,?,?,?,?,?,?,?,?,?,?)");
+public function storeCustomerOrderInvoice($invoice_number, $customer_name, $orderdate, $find_customer_name, $total_quantity, $orderQuantity, $price, $totalPrice, $pro_name, $pid, $subtotal, $prev_due, $netTotal, $paidBill, $dueBill, $payMethode) {
 
-		$pre_stmt->bindParam(1,$invoice_number, PDO::PARAM_STR);
-		$pre_stmt->bindParam(2,$customer_name, PDO::PARAM_INT);
-		$pre_stmt->bindParam(3,$find_customer_name, PDO::PARAM_STR);
-		$pre_stmt->bindParam(4,$orderdate, PDO::PARAM_STR);
-		$pre_stmt->bindParam(5,$subtotal, PDO::PARAM_STR);
-		$pre_stmt->bindParam(6,$discount, PDO::PARAM_STR);
-		$pre_stmt->bindParam(7,$prev_due, PDO::PARAM_STR);
-		$pre_stmt->bindParam(8,$netTotal, PDO::PARAM_STR);
-		$pre_stmt->bindParam(9,$paidBill, PDO::PARAM_STR);
-		$pre_stmt->bindParam(10,$dueBill, PDO::PARAM_STR);
-		$pre_stmt->bindParam(11,$payMethode, PDO::PARAM_STR);
-		$pre_stmt->execute() or die("failed");
-		$stmt = $this->pdo->query("SELECT LAST_INSERT_ID()");
-		$invoice_no = $stmt->fetchColumn();
+	$pre_stmt = $this->pdo->prepare("INSERT INTO `invoice`(`invoice_number`, `customer_id`, `customer_name`, `order_date`, `sub_total`, `pre_cus_due`, `net_total`, `paid_amount`, `due_amount`, `payment_type`) VALUES (?,?,?,?,?,?,?,?,?,?)");
 
-		if ($invoice_no != null) {
-			for ($i=0; $i <count($price) ; $i++) { 
-				// substract quantity
-				$remain_quantity = $total_quantity[$i] - $orderQuantity[$i];
-				if ($remain_quantity < 0) {
-					return "Sorry ! you haven't the quantity";
-				}else{
-					$stmt = $this->pdo->prepare("UPDATE `products` SET `quantity`=$remain_quantity WHERE `id` = $pid[$i]");
-					$stmt->bindParam(":value", $value);
-					$stmt->execute();
-				}
+	$pre_stmt->bindParam(1, $invoice_number, PDO::PARAM_STR);
+	$pre_stmt->bindParam(2, $customer_name, PDO::PARAM_INT);
+	$pre_stmt->bindParam(3, $find_customer_name, PDO::PARAM_STR);
+	$pre_stmt->bindParam(4, $orderdate, PDO::PARAM_STR);
+	$pre_stmt->bindParam(5, $subtotal, PDO::PARAM_STR);
+	$pre_stmt->bindParam(6, $prev_due, PDO::PARAM_STR);
+	$pre_stmt->bindParam(7, $netTotal, PDO::PARAM_STR);
+	$pre_stmt->bindParam(8, $paidBill, PDO::PARAM_STR);
+	$pre_stmt->bindParam(9, $dueBill, PDO::PARAM_STR);
+	$pre_stmt->bindParam(10, $payMethode, PDO::PARAM_STR);
+	$pre_stmt->execute() or die("failed");
 
-				$insert_product = $this->pdo->prepare("INSERT INTO `invoice_details`(`invoice_no`,`pid`, `product_name`, `price`, `quantity`) VALUES (?,?,?,?,?)");
-				$insert_product->bindParam(1,$invoice_no,PDO::PARAM_INT);
-				$insert_product->bindParam(2,$pid[$i],PDO::PARAM_INT);
-				$insert_product->bindParam(3,$pro_name[$i],PDO::PARAM_STR);
-				$insert_product->bindParam(4,$totalPrice[$i],PDO::PARAM_STR);
-				$insert_product->bindParam(5,$orderQuantity[$i],PDO::PARAM_STR);
-				$insert_product->execute();
+	$stmt = $this->pdo->query("SELECT LAST_INSERT_ID()");
+	$invoice_no = $stmt->fetchColumn();
+
+	if ($invoice_no != null) {
+		for ($i = 0; $i < count($price); $i++) {
+			// subtract quantity
+			$remain_quantity = $total_quantity[$i] - $orderQuantity[$i];
+			if ($remain_quantity < 0) {
+				return "Sorry! You don't have enough quantity.";
+			} else {
+				$stmt = $this->pdo->prepare("UPDATE `products` SET `quantity` = $remain_quantity WHERE `id` = $pid[$i]");
+				$stmt->execute();
 			}
-			// payment table update 
-			
-			$payment_query = array(
-		    	'customer_id' => $customer_name,
-		    	'payment_date' => $orderdate,
-		    	'payment_amount' => $paidBill,
-		    	'payment_type' => $payMethode,
-		    	'added_by' => $_SESSION['user_id'],
-			);
-			$payment_res = $this->create('sell_payment',$payment_query);
-			// find customer previous transition 
-			$find_customer_stmt = $this->find('member','id',$customer_name);
-			$prev_total_buy = $find_customer_stmt->total_buy;
-			$prev_total_paid = $find_customer_stmt->total_paid;
-			$new_total_buy = $prev_total_buy + $subtotal;
-		    $new_total_paid = $prev_total_paid + $paidBill;
 
-		    $member_update_query = array(
-				'total_buy' => $new_total_buy,
-				'total_paid' => $new_total_paid,
-				'total_due' => $dueBill,
-			);
-
-			$suppliar_update_res = $this->update('member','id' , $customer_name,$member_update_query);
-		
-
-			return $invoice_no;
+			$insert_product = $this->pdo->prepare("INSERT INTO `invoice_details`(`invoice_no`, `pid`, `product_name`, `price`, `quantity`) VALUES (?,?,?,?,?)");
+			$insert_product->bindParam(1, $invoice_no, PDO::PARAM_INT);
+			$insert_product->bindParam(2, $pid[$i], PDO::PARAM_INT);
+			$insert_product->bindParam(3, $pro_name[$i], PDO::PARAM_STR);
+			$insert_product->bindParam(4, $totalPrice[$i], PDO::PARAM_STR);
+			$insert_product->bindParam(5, $orderQuantity[$i], PDO::PARAM_STR);
+			$insert_product->execute();
 		}
 
+		// payment table update 
+		$payment_query = array(
+			'customer_id' => $customer_name,
+			'payment_date' => $orderdate,
+			'payment_amount' => $paidBill,
+			'payment_type' => $payMethode,
+			'added_by' => $_SESSION['user_id'],
+		);
+		$payment_res = $this->create('sell_payment', $payment_query);
+
+		// update customer transition totals
+		$find_customer_stmt = $this->find('member', 'id', $customer_name);
+		$prev_total_buy = $find_customer_stmt->total_buy;
+		$prev_total_paid = $find_customer_stmt->total_paid;
+
+		$new_total_buy = $prev_total_buy + $subtotal;
+		$new_total_paid = $prev_total_paid + $paidBill;
+
+		$member_update_query = array(
+			'total_buy' => $new_total_buy,
+			'total_paid' => $new_total_paid,
+			'total_due' => $dueBill,
+		);
+
+		$suppliar_update_res = $this->update('member', 'id', $customer_name, $member_update_query);
+
+		return $invoice_no;
+	}
 }
-
-
-} //end of class
-
-?>
+}
